abe33bec7f55d73dd072c8506ea1b934105b91e3
perosa <beppe.catanese@gmail.com>
2020-05-07 15:57:46 +0200

    Pass platform as param

--- world_capitals_bot/metrics/bot_metrics_connector.py
+++ world_capitals_bot/metrics/bot_metrics_connector.py
@@ -1,14 +1,14 @@
 import requests, os, logging
 
 
-def send_metrics(text, user_id):
+def send_metrics(text, user_id, platform):
 
     api_key = get_api_key()
 
     if api_key is not None:
         p = {'token': api_key}
         headers = {'Content-Type': 'application/json'}
-        json = get_json(text, user_id)
+        json = get_json(text, user_id, platform)
 
         r = requests.post("https://api.bot-metrics.com/v1/messages", params=p,
                       json=json, headers=headers)
@@ -16,8 +16,8 @@ def send_metrics(text, user_id):
         logging.debug(r)
 
 
-def get_json(text, user_id):
-    data = {'text': text, 'message_type': 'incoming', 'user_id': user_id, 'platform': 'telegram'}
+def get_json(text, user_id, platform):
+    data = {'text': text, 'message_type': 'incoming', 'user_id': user_id, 'platform': platform}
 
     return data
 
